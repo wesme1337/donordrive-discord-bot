59fb1ad19041280a81c5fcf6c66cb4a947b9c21a
wesme1337 <wesme1337@gmail.com>
2018-05-11 16:31:05 -0400

    fix errors and save work

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const Discord = require('discord.js');
-const axios = require('axios';);
+const axios = require('axios');
 const client = new Discord.Client();
-const config = require('config.json');
+const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('Ready!');
 });
-
-if (message.contet === '!topDonations') {
-
-}
+client.on('message', message => {
+	if (message.content === '!topDonations') {
+		axios({
+			method: 'get',
+			url: 'https://try.donordrive.com/api/participants'
+		}).then(function(response){
+			// log call in db
+			message.channel.send({embed : {
+				color: 3447003,
+				description:
+			}})
+		})
+	}
+})
 
 client.login(config.token);
